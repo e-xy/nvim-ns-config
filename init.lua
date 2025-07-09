36fc5027b4ba784ddbfab6b6416dc5361803585a
Ericxiyinyang <ericxiyin@gmail.com>
2025-07-08 19:14:30 -0700

    disable copilot in prep for avante

--- init.lua
+++ init.lua
@@ -49,50 +49,50 @@ require("lspconfig").clangd.setup({
   },
 })
 
-require("copilot").setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>",
-    },
-    layout = {
-      position = "bottom", -- | top | left | right | horizontal | vertical
-      ratio = 0.4,
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = false,
-    hide_during_completion = true,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = "node", -- Node.js version must be > 18.x
-  server_opts_overrides = {},
-})
+-- require("copilot").setup({
+--   panel = {
+--     enabled = true,
+--     auto_refresh = false,
+--     keymap = {
+--       jump_prev = "[[",
+--       jump_next = "]]",
+--       accept = "<CR>",
+--       refresh = "gr",
+--       open = "<M-CR>",
+--     },
+--     layout = {
+--       position = "bottom", -- | top | left | right | horizontal | vertical
+--       ratio = 0.4,
+--     },
+--   },
+--   suggestion = {
+--     enabled = true,
+--     auto_trigger = false,
+--     hide_during_completion = true,
+--     debounce = 75,
+--     keymap = {
+--       accept = "<M-l>",
+--       accept_word = false,
+--       accept_line = false,
+--       next = "<M-]>",
+--       prev = "<M-[>",
+--       dismiss = "<C-]>",
+--     },
+--   },
+--   filetypes = {
+--     yaml = false,
+--     markdown = false,
+--     help = false,
+--     gitcommit = false,
+--     gitrebase = false,
+--     hgcommit = false,
+--     svn = false,
+--     cvs = false,
+--     ["."] = false,
+--   },
+--   copilot_node_command = "node", -- Node.js version must be > 18.x
+--   server_opts_overrides = {},
+-- })
 
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font"
